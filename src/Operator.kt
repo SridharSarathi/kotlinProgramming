76af43e47901fde04dae6d5d1af3e9a439ac8390
SRI <sridharrajavel1506@gmail.com>
2024-12-17 17:04:12 +0530

    Operators done...!

---
+++ src/Operator.kt
@@ -0,0 +1,45 @@
+fun main()
+{
+    //"Arithmetric"
+    println("Arithemetric Operator");
+    var d=43;
+    println(5+9);
+    println(4-9);
+    println(12*6);
+    println(32/3);
+    println(43%2);
+    println(d++);
+    println(d--);
+    println(++d);
+    println(--d);
+    //
+    println("Assingment Operator");
+    //Assingment Operator
+    var a=8;
+    a+=9;println(a);
+    a-=5;println(a);
+    a/=3;println(a);
+    a*=4;println(a);
+    a%=2;println(a);
+
+    //Logical Operator
+    var b=89;
+    println(99>b&&b<89);
+    println(99>b||b<89);
+    var bool=false;
+    println(!bool);
+
+    //comparison Operator
+    println("Comparison Operator");
+    val p=23;val m=88;
+    println('p'+a);
+    println('m'+b);
+    println("23 equals to 88 "+(p==m));
+    println("23 not equal 88 "+(p!=m));
+    println("23 less than or equal 88 "+(p<=m));
+    println("23 greater than or equal 88 "+(p>=m));
+    println("23 less than or equal 88 "+(p>m));
+    println("23 greater tha 88 "+(p>m));
+
+
+}
