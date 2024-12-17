faca3064ab19c2239221055a5e3a3c61cf516f81
SRI <sridharrajavel1506@gmail.com>
2024-12-17 12:59:48 +0530

    Datatypes

---
+++ src/DataTypes.kt
@@ -0,0 +1,15 @@
+fun main() {
+    //val or var variablename=value;
+    var name= "Sridhar";//String
+    val num=90;//Int
+    var fra=89.0f;//Float
+    var dou=8.09;//Double
+    var initial='R';//Char
+    var lon=4322344904504L;//long
+
+    print(name+"\n"+num+"\n"+fra+"\n"+dou+"\n"+initial+"\n"+lon);
+
+
+
+
+}
