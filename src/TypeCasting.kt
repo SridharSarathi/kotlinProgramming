6cdce95faef834db95625c3ab4b901979bf20acc
SRI <sridharrajavel1506@gmail.com>
2024-12-17 13:34:05 +0530

    TypeCasting Methods

---
+++ src/TypeCasting.kt
@@ -0,0 +1,21 @@
+fun main()
+{   //Int to Float
+    var x=90809800;
+    var c=x.toFloat();
+    println(c);
+    //char to String
+    var C='s';
+    var s=C.toString();
+    //String to Char
+    var str=77;
+    var ch=str.toChar();
+    print(ch)
+    //methods
+//    toString()
+//    toChar()
+//    toByte()
+//    toLong()
+//    toShort()
+//    toDouble()
+//    toFloat()
+}
