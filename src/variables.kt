c19120e750b6a0ca1fe040df9396732f6d794aec
SRI <sridharrajavel1506@gmail.com>
2024-12-17 12:13:13 +0530

    kotlin programming

---
+++ src/variables.kt
@@ -0,0 +1,11 @@
+
+    fun main() {
+
+            val name = "Sridhar";
+            var age = 23;
+            val weight = 87.8;
+            var int = 'R';
+            var pass = true;
+        print(name+"\n"+weight+"\n"+int+"\n"+pass);
+        }
+
