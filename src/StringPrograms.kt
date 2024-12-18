9b55691821c088d5bbc7887853caea4328117609
sridhar <sridharrajavel1506@gmail.com>
2024-12-18 17:37:40 +0530

    String methods Added

--- src/StringPrograms.kt
+++ src/StringPrograms.kt
@@ -1,11 +1,10 @@
-fun main()
-{
+fun main() {
     //String Programs
-    val Str="Lenovo";
-    println("String length is "+Str.length);
+    val Str = "Lenovo";
+    println("String length is " + Str.length);
     //Declaration After Using
-    var name:String;
-    name="Github";
+    var name: String;
+    name = "Github";
     println(name);
     //Access
     println(name[2]);
@@ -17,13 +16,29 @@ fun main()
     println("Value".indexOf("e"))
     println("Display's");
     //Concatination
-    println("William"+" "+"Shakespear");
+    println("William" + " " + "Shakespear");
     println("William".plus(" Edwick"));
+    println("Vikings".plus(" Come from").plus(" North"));
 
 //    String Templates/Interpolation using $sign
     println("Your Name is $Str");
     //Substring
-    println(Str.substring(2,5));
-    val r=89;
+    println(Str.substring(2, 5));
+    val r = 89;
     println("Age ${r}");
+
+    var cv = "Accessable".toByteArray();
+    println(cv.contentToString());
+    //Squences of String
+    var S1 = "Programs".subSequence(0, 2);
+    println(S1);
+    println(S1.count());
+// Split()
+    var sp="Mango,Apple,Orange,Banana";
+    println(sp.split(","));
+    //Replace
+    println("Hello Bro".replace("Bro","World"))
+    println("Benene".replaceAfter("e","s")); //values Changed after e
+    println("     Alphapetics      ".trim());
+
 }
